0d2a5519145a915498019584a9296f364a6e259e
Magnus Samuelsson <magnus.samuelsson@vericate.se>
2024-12-03 16:41:47 +0100

    Initial Commit

---
+++ day1.php
@@ -0,0 +1,26 @@
+<?php
+$input = file_get_contents('inputs/day1.txt');
+$input = explode("\n", $input);
+$leftList = [];
+$rightList = [];
+foreach ($input as $line) {
+    $numbers = preg_split('/\s+/', $line);
+    $leftList[] = $numbers[0];
+    $rightList[] = $numbers[1];
+}
+
+sort($leftList);
+sort($rightList);
+
+$sumPart1 = 0;
+$sumPart2 = 0;
+$rightListCounter = array_count_values($rightList);
+foreach($leftList as $key => $value) {
+    $sumPart1 += abs($value - $rightList[$key]);
+    if (array_key_exists($value, $rightListCounter)) {
+        $sumPart2 +=  $value * $rightListCounter[$value];
+    }
+}
+
+echo "<p>The answer for part 1 is: $sumPart1";
+echo "<p>The answer for part 2 is: $sumPart2";
---
+++ day2.php
@@ -0,0 +1,78 @@
+<?php
+$input = file_get_contents('inputs/day2.txt');
+$input = preg_split('/\r?\n/', $input);
+$inputArray = [];
+foreach ($input as $line) {
+    $inputArray[] = preg_split('/\s+/', trim($line));
+}
+function checkChange($array) {
+    $increasing     = true;
+    $decreasing     = true;
+
+    for ( $i = 1; $i < count( $array ); $i ++ ) {
+        $diff = $array[ $i ] - $array[ $i - 1 ];
+        if ( abs( $diff ) < 1 || abs( $diff ) > 3 ) {
+            return false;
+        }
+        if ( $diff > 0 ) {
+            $decreasing = false;
+        } elseif ( $diff < 0 ) {
+            $increasing = false;
+        }
+        if (!( $increasing || $decreasing )) {
+            return false;
+        }
+    }
+    return true;
+}
+function checkChange2($array) {
+    global $part1Counter;
+    $increasing     = true;
+    $decreasing     = true;
+
+    for ( $i = 1; $i < count( $array ); $i ++ ) {
+        $diff = $array[ $i ] - $array[ $i - 1 ];
+        if ( abs( $diff ) < 1 || abs( $diff ) > 3 ) {
+            return tryRemoveOneIndex($array, $i);
+        }
+        if ( $diff > 0 ) {
+            $decreasing = false;
+        } elseif ( $diff < 0 ) {
+            $increasing = false;
+        }
+        if (!( $increasing || $decreasing )) {
+            return tryRemoveOneIndex($array, $i);
+        }
+    }
+    $part1Counter++;
+    return true;
+}
+function tryRemoveOneIndex($array, $index) {
+    global $part2Counter;
+    $levelsCopy = $array;
+    unset( $levelsCopy[ $index ] );
+    if ( checkChange( array_values( $levelsCopy ) ) ) {
+        $part2Counter++;
+        return true;
+    }
+    $levelsCopy = $array;
+    unset( $levelsCopy[ $index - 1 ] );
+    if ( checkChange( array_values( $levelsCopy ) ) ) {
+        $part2Counter++;
+        return true;
+    }
+    $levelsCopy = $array;
+    unset( $levelsCopy[ 0 ] );
+    if ( checkChange( array_values( $levelsCopy ) ) ) {
+        $part2Counter++;
+        return true;
+    }
+    return false;
+}
+$part1Counter = 0;
+$part2Counter = 0;
+foreach ($inputArray as $key => $value) {
+    checkChange2($value);
+}
+echo "<p>The answer for part 1 is: $part1Counter";
+echo "<p>The answer for part 2 is: ".$part1Counter+$part2Counter;
